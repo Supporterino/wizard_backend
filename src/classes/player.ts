57b594827cc60edb740ca383af356129c5c8e6b3
Supporterino <lars@roth-kl.de>
2021-01-06 16:08:10 +0000

    First lines of player

--- src/classes/player.ts
+++ src/classes/player.ts
@@ -1,3 +1,11 @@
+import { Card } from "./card";
+
 export class Player {
-    
+    private hand: Array<Card>;
+    private id: string;
+
+    constructor(inID: string) {
+        this.id = inID;
+        this.hand = new Array<Card>();
+    }
 }

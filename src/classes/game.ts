fc2f56fab25fc7fda0a3bc6cc77e3cbc3da8c91d
Supporterino <lars@roth-kl.de>
2021-01-07 20:43:45 +0000

    WIP Turn ending

--- src/classes/game.ts
+++ src/classes/game.ts
@@ -8,12 +8,11 @@ export class Game {
     private players: Array<Player>;
     private roundCounter: number;
     private round!: Round;
-    private activePlayer!: Player;
 
     constructor() {
         this.id = gameID();
         this.players = new Array<Player>();
-        this.roundCounter = 1;
+        this.roundCounter = 3;
     }
 
     addPlayer(player: Player): void {
@@ -23,29 +22,24 @@ export class Game {
     startNewRound(): void {
         this.round = new Round(this.roundCounter, this.players);
         this.round.start();
-        this.roundCounter++;
-        this.activePlayer = this.players[0];
     }
 
     playTurn(playerID: string, card: Card): void {
-        if (this.activePlayer.getID() === playerID) {
-            const toPlay = this.activePlayer.playCard(card);
-            this.round.addCardToPile(toPlay);
-
-            if (this.activePlayer != this.players[this.players.length - 1]) this.activePlayer = this.players[this.players.indexOf(this.activePlayer) + 1];
-            else this.endRound();
+        let end = this.round.playTurn(playerID, card);
+        if (end) {
+            this.endRound();
         }
     }
 
     endRound(): void {
         console.log(`Round ${this.roundCounter} end.`);
+        this.roundCounter++;
     }
 
     toString(): string {
         let output = `Game{id: ${this.id}, \n`;
         output += `counter: ${this.roundCounter}, \n`;
         output += `round: ${this.round.toString()}, \n`;
-        output += `activePlayer: ${this.activePlayer.getID()}, \n`;
         output += `players: [ \n`;
         for (const player of this.players) {
             output += `\t ${player.toString()}, \n`;
--- src/classes/round.ts
+++ src/classes/round.ts
@@ -7,12 +7,18 @@ export class Round {
     private deck: Deck;
     private players: Array<Player>;
     private pile: Array<Card>;
+    private turnCounter: number;
+    private turns: number;
+    private activePlayer: Player;
 
     constructor(n: number, plrs: Array<Player>) {
         this.num = n;
         this.players = plrs;
+        this.turnCounter = 0;
+        this.turns = n;
         this.deck = new Deck();
         this.pile = new Array<Card>();
+        this.activePlayer = this.players[0];
     }
 
     start() {
@@ -25,6 +31,22 @@ export class Round {
         }
     }
 
+    playTurn(playerID: string, card: Card): boolean {
+            const toPlay = this.activePlayer.playCard(card);
+            this.addCardToPile(toPlay);
+    
+            if (this.activePlayer != this.players[this.players.length - 1]) {
+                this.activePlayer = this.players[this.players.indexOf(this.activePlayer) + 1];
+                return false;
+            } else {
+                return true;
+            }
+    }
+
+    analyzeTurn() {
+        
+    }
+
     addCardToPile(card: Card): void {
         this.pile.push(card);
     }
@@ -35,7 +57,8 @@ export class Round {
             output += `\t ${card.toString()} \n`;
         }
         output += `], \n'`;
-        output += `${this.deck.toString()}}`;
+        output += `${this.deck.toString()}, \n`;
+        output += `activePlayer: ${this.activePlayer.getID()}}`;
         return output;
     }
 }

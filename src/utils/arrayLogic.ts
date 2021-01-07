f637939bce8413820ba5cfd5d00510de0e090f1c
Supporterino <lars@roth-kl.de>
2021-01-07 15:31:37 +0000

    Fixed hand clearing

--- src/utils/arrayLogic.ts
+++ src/utils/arrayLogic.ts
@@ -1,5 +1,5 @@
 export const removeElement = (arr: Array<any>, ele: any): any => {
     const index = arr.indexOf(ele);
-    if (index > -1) return arr.slice(index, 1)[0];
+    if (index > -1) return arr.splice(index, 1)[0];
     else return undefined;
 }
